2e2121e655dc95244ad0510976eeefafcd0dab57
o-Jenny <hyeon2_0@naver.com>
2022-08-30 16:46:58 +0900

    ㄱㄱㄱㄱㄱ

--- src/main/java/service/CategoryMybatisDao.java
+++ src/main/java/service/CategoryMybatisDao.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Repository;
 
-import model.Board;
 import model.Category;
 
 
